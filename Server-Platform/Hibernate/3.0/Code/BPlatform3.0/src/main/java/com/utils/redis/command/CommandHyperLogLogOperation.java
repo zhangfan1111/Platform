6ece42a3ef6446ee9039ec76e6ca01399980cdf8
Memory <302617753@qq.com>
2017-06-06 10:38:08 +0800

    commit HyperLogLogOperations operations

--- Server-Platform/Hibernate/3.0/Code/BPlatform3.0/src/main/java/com/utils/redis/command/CommandHyperLogLogOperation.java
+++ Server-Platform/Hibernate/3.0/Code/BPlatform3.0/src/main/java/com/utils/redis/command/CommandHyperLogLogOperation.java
@@ -2,9 +2,10 @@ package com.utils.redis.command;
 
 /**
  * Interface HyperLogLog Command Operations.
- * <p>
- * http://doc.redisfans.com/
- * </p>
+ * <ul>
+ * <li>http://doc.redisfans.com/</li>
+ * <li>http://www.runoob.com/redis</li>
+ * </ul>
  * 
  * @author memory 2017年5月11日 下午9:04:47
  * @version V1.0
@@ -14,4 +15,63 @@ package com.utils.redis.command;
  */
 public interface CommandHyperLogLogOperation {
 
+	/**
+	 * HyperLogLog 实现命令：PFADD key element [element ...]。将所有元素参数添加到 HyperLogLog 数据结构中。
+	 * 
+	 * <p>
+	 * 可用版本：>= 2.8.9
+	 * </p>
+	 * 
+	 * @author memory 2017年6月6日 上午10:18:29
+	 * @param key
+	 * @param values
+	 * @return 如果至少有个元素被添加返回 1， 否则返回 0。
+	 */
+	Long PFADD(String key, String... values);
+
+	/**
+	 * HyperLogLog 实现命令：PFCOUNT key [key ...]。返回给定 HyperLogLog 的基数估算值。
+	 * 
+	 * <ul>
+	 * <li>redis 127.0.0.1:6379> PFADD hll foo bar zap</li>
+	 * <li>(integer) 1</li>
+	 * <li>redis 127.0.0.1:6379> PFADD hll zap zap zap</li>
+	 * <li>(integer) 0</li>
+	 * <li>redis 127.0.0.1:6379> PFADD hll foo bar</li>
+	 * <li>(integer) 0</li>
+	 * <li>redis 127.0.0.1:6379> PFCOUNT hll</li>
+	 * <li>(integer) 3</li>
+	 * <li>redis 127.0.0.1:6379> PFADD some-other-hll 1 2 3</li>
+	 * <li>(integer) 1</li>
+	 * <li>redis 127.0.0.1:6379> PFCOUNT hll some-other-hll</li>
+	 * <li>(integer) 6</li>
+	 * </ul>
+	 * 
+	 * @author memory 2017年6月6日 上午10:27:25
+	 * @param keys
+	 * @return 返回给定 HyperLogLog 的基数值，如果多个 HyperLogLog 则返回基数估值之和。Gets the current number of elements within the key.
+	 */
+	Long PFCOUNT(String... keys);
+	
+	/**
+	 * HyperLogLog 实现命令：PFDEL key。根据key删除相关值。
+	 * 
+	 * @author memory 2017年6月6日 上午10:32:27
+	 * @param key
+	 */
+	void PFDEL(String key);
+	
+	/**
+	 * HyperLogLog 实现命令：PFMERGE destkey sourcekey [sourcekey ...]。Merges all values of given sourceKeys into destination key.
+	 * 
+	 * <p>
+	 * 将多个 HyperLogLog 合并为一个 HyperLogLog ，合并后的 HyperLogLog 的基数估算值是通过对所有 给定 HyperLogLog 进行并集计算得出的。
+	 * </p>
+	 * 
+	 * @author memory 2017年6月6日 上午10:33:42
+	 * @param destinationKey
+	 * @param sourceKeys
+	 * @return
+	 */
+	Long PFMERGE(String destinationKey, String... sourceKeys);
 }
--- Server-Platform/Hibernate/3.0/Code/BPlatform3.0/src/main/java/com/utils/redis/command/DefaultCommandHyperLogLogOperation.java
+++ Server-Platform/Hibernate/3.0/Code/BPlatform3.0/src/main/java/com/utils/redis/command/DefaultCommandHyperLogLogOperation.java
@@ -4,8 +4,22 @@ import org.springframework.data.redis.core.HyperLogLogOperations;
 
 /**
  * HyperLogLog Command Operations.extends RedisCommandOperation.
+ * <ul>
+ * <li>http://doc.redisfans.com/</li>
+ * <li>http://www.runoob.com/redis</li>
+ * </ul>
+ * 
  * <p>
- * http://doc.redisfans.com/
+ * Redis 在 2.8.9 版本添加了 HyperLogLog 结构。
+ * </p>
+ * <p>
+ * Redis HyperLogLog 是用来做基数统计的算法，HyperLogLog 的优点是，在输入元素的数量或者体积非常非常大时，计算基数所需的空间总是固定 的、并且是很小的。
+ * </p>
+ * <p>
+ * 在 Redis 里面，每个 HyperLogLog 键只需要花费 12 KB 内存，就可以计算接近 2^64 个不同元素的基 数。这和计算基数时，元素越多耗费内存就越多的集合形成鲜明对比。
+ * </p>
+ * <p>
+ * 但是，因为 HyperLogLog 只会根据输入元素来计算基数，而不会储存输入元素本身，所以 HyperLogLog 不能像集合那样，返回输入的各个元素。
  * </p>
  * 
  * @author memory 2017年5月11日 下午9:01:49
@@ -19,4 +33,24 @@ public class DefaultCommandHyperLogLogOperation extends DefaultCommandRedisOpera
 	 * Interface HyperLogLogOperations<K,V>.
 	 */
 	protected HyperLogLogOperations<String, String> hllOps = super.opsForHyperLogLog();
+
+	@Override
+	public Long PFADD(String key, String... values) {
+		return hllOps.add(key, values);
+	}
+
+	@Override
+	public Long PFCOUNT(String... keys) {
+		return hllOps.size(keys);
+	}
+
+	@Override
+	public void PFDEL(String key) {
+		hllOps.delete(key);
+	}
+
+	@Override
+	public Long PFMERGE(String destinationKey, String... sourceKeys) {
+		return hllOps.union(destinationKey, sourceKeys);
+	}
 }
